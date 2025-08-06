21a5049431db3102f82502ce6f7a41f700c69626
Thomashoang99 <40379418+Thomashoang99@users.noreply.github.com>
2025-08-06 21:07:05 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',  //public
+    //add more here
+  ]
+}
 
 const app = express();
-app.use(cors(), express.json());
+app.use(cors(corsOptions)).use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // DB
 mongoose.connect(process.env.MONGO_URI)
